69709fc2299451916d0da15ca83a9ebd026232fa
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2019-09-16 19:37:50 -0700

    Mock application interface for testing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 // @flow strict
-import React, {useState} from 'react';
-import classNames from 'classnames';
-import Scroller, {Themes, Orientations} from './Scroller';
+import React, {useState, useEffect, type Node} from 'react';
+import Scroller, {Themes} from './Scroller';
 import styles from './App.module.css';
+
+const AppThemes = Object.freeze({
+  DARK: 'dark',
+  LIGHT: 'light',
+});
+
+type ThemeFakerProps = {|
+  theme: $Values<typeof AppThemes>,
+|};
+
+function ThemeFaker({theme}: ThemeFakerProps) {
+  useEffect(
+    () => {
+      const {documentElement} = document;
+      if (documentElement == null) {
+        return;
+      }
+      documentElement.className = `theme-${theme}`;
+    },
+    [theme]
+  );
+  return null;
+}
 
 type SelectProps<Value> = {|
   value: Value,
@@ -22,45 +44,53 @@ function Select<Value>({value, setValue, base}: SelectProps<Value>) {
   );
 }
 
-// There are a few various contexts we need to be able to support
-// * Chrome - supports custom scrollbar styles which automatically pad the
-// content for us
-// * Firefox - Latest version of firefox supports custom scrollbars but on mac,
-// depending on settings the scrollbar may or may not pad the content, so we
-// need to detect whether the content is padded or not from the custom
-// scrollbar styles
-// * All other browsers - if the custom css scrollbar styles affect the
-// scrollbar within the desired range then we assume it looks correct,
-// otherwise we need to add additional padding to account for the scrollbar
+type FakeDataProps = {|
+  count: number,
+  className: string,
+  text?: ?string,
+|};
+
+function FakeData({count, className, text}: FakeDataProps) {
+  return new Array(count).fill(null).map((_, key) => (
+    <div className={className} key={key}>
+      {text}
+    </div>
+  ));
+}
+
+type FloaterProps = {|
+  children: Node,
+|};
+
+function Floater({children}: FloaterProps) {
+  return (
+    <Scroller className={styles.floater} theme={Themes.THINN}>
+      {children}
+      <FakeData count={20} className={styles.floatingItem} />
+    </Scroller>
+  );
+}
 
 export default () => {
-  const [orientation, setOrientation] = useState<$Values<typeof Orientations>>(Orientations.VERTICAL);
-  const [theme, setTheme] = useState<$Values<typeof Themes>>(Themes.THINN);
-  const [fade, setFade] = useState<boolean>(false);
+  const [appTheme, setAppTheme] = useState<$Values<typeof AppThemes>>(AppThemes.DARK);
   return (
-    <>
-      <div className={styles.tools}>
-        <Select value={orientation} setValue={setOrientation} base={Orientations} />
-        <Select value={theme} setValue={setTheme} base={Themes} />
-        <label>
-          <input type="checkbox" value={fade} onChange={({currentTarget}) => setFade(currentTarget.checked)} /> Fade
-        </label>
-      </div>
-      <Scroller
-        fade={fade}
-        theme={theme}
-        orientation={orientation}
-        className={classNames({
-          [styles.container]: true,
-          [styles.vertical]: orientation === Orientations.VERTICAL,
-          [styles.horizontal]: orientation === Orientations.HORIZONTAL,
-        })}>
-        {new Array(25).fill(null).map((_, key) => (
-          <div className={styles.item} key={key}>
-            An Item
-          </div>
-        ))}
+    <div className={styles.app}>
+      <Scroller className={styles.guildList} theme={Themes.NONEE}>
+        <FakeData count={25} className={styles.box} />
       </Scroller>
-    </>
+      <Scroller className={styles.channelList} theme={Themes.THINN} fade>
+        <FakeData count={100} className={styles.rowItem} text="Channel" />
+      </Scroller>
+      <Scroller className={styles.messages} theme={Themes.THICC}>
+        <FakeData count={25} className={styles.rowItemBig} />
+      </Scroller>
+      <Scroller className={styles.memberList} theme={Themes.THINN} fade>
+        <FakeData count={100} className={styles.rowItem} text="Member" />
+      </Scroller>
+      <Floater>
+        <Select value={appTheme} setValue={setAppTheme} base={AppThemes} />
+      </Floater>
+      <ThemeFaker theme={appTheme} />
+    </div>
   );
 };
