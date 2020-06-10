97b6a9c0eff05252c265f5629d8e7a1b6555d288
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2020-06-10 12:02:08 -0700

    Rename CSSScroller to SimpleScroller

--- src/factories/createSimpleScroller.tsx
+++ src/factories/createSimpleScroller.tsx
@@ -2,17 +2,17 @@ import React, {useRef} from 'react';
 import {usePaddingFixes, getScrollbarSpecs, getMergedOrientationStyles} from '../scroller-utilities';
 import type {ScrollerComponentBaseProps, ScrollerOrientationTypes} from '../scroller-utilities';
 
-// CSSScroller is a component that does nothing but allow tall content to
+// SimpleScroller is a component that does nothing but allow tall content to
 // overflow and scroll.  No need for ref handling, or scrolling state.  It's
 // about as bare bones as you need to be, with all the padding fixes included.
 // Most of the time you probably want to use this
 
-export interface CSSScrollerProps extends ScrollerComponentBaseProps {
+export interface SimpleScrollerProps extends ScrollerComponentBaseProps {
   orientation?: ScrollerOrientationTypes;
   children: React.ReactNode;
 }
 
-export default function createScroller(scrollbarClassName?: string) {
+export default function createSimpleScroller(scrollbarClassName?: string) {
   const specs = getScrollbarSpecs(scrollbarClassName);
   return function CSSScroller({
     children,
@@ -22,13 +22,15 @@ export default function createScroller(scrollbarClassName?: string) {
     paddingFix = true,
     style,
     ...props
-  }: CSSScrollerProps) {
+  }: SimpleScrollerProps) {
     const scrollerRef = useRef<HTMLDivElement>(null);
     const paddingNode = usePaddingFixes({paddingFix, orientation, dir, className, scrollerRef, specs});
-    const classes = [scrollbarClassName, className].filter((str) => str != null);
-    const mergedStyles = getMergedOrientationStyles(orientation, style);
     return (
-      <div ref={scrollerRef} className={classes.join(' ')} style={mergedStyles} {...props}>
+      <div
+        ref={scrollerRef}
+        className={[scrollbarClassName, className].filter((str) => str != null).join(' ')}
+        style={getMergedOrientationStyles(orientation, style)}
+        {...props}>
         {children}
         {paddingNode}
       </div>
